14d7dbc86dd5dcfe964e5edc268915f1a4065a6d
yezii <1126yezy@gmail.com>
2021-03-02 17:12:51 +0900

    Router Setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
+
 function App() {
-  return <div></div>;
+  const isLoggedIn = false;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {isLoggedIn ? <Home /> : <Login />}
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
