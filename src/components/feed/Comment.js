4becf55ffa100398e2068a65c04ef6ac6cf66611
yezii <1126yezy@gmail.com>
2021-03-07 15:14:55 +0900

    Parsing Hashtags from caption

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,6 +1,8 @@
+import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { FatText } from "../shared";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 
@@ -12,13 +14,28 @@ const CommentUsername = styled(FatText)`
 
 const CommentCaption = styled.span`
   margin-left: 5px;
+  a {
+    background-color: inherit;
+    color: rgb(0, 55, 107);
+    cursor: pointer;
+  }
 `;
 
 function Comment({ author, caption }) {
   return (
     <CommentContainer>
       <CommentUsername>{author}</CommentUsername>
-      <CommentCaption>{caption}</CommentCaption>
+      <CommentCaption>
+        {caption.split(" ").map((word, index) =>
+          /#[\w]+/g.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word}</React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
